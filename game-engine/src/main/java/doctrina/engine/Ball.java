9856eff6ad952d57a76b2a60c77e5ee04bee4c2e
William Beaudin <williambeaudun@gmail.com>
2024-09-04 15:05:19 -0400

    Refactor: Extracted ball class

---
+++ game-engine/src/main/java/doctrina/engine/Ball.java
@@ -0,0 +1,75 @@
+package doctrina.engine;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.Random;
+
+public class Ball {
+
+    public static final int DEFAULT_SPEED = 5;
+
+    private int x, y, dx, dy, radius, speed;
+
+    public Ball(int radius, int speed) {
+        this.radius = radius;
+        this.speed = speed;
+        initPos();
+    }
+
+    public Ball(int radius) {
+        this(radius, DEFAULT_SPEED);
+    }
+
+    private void initPos() {
+        this.x = randomNumber(this.radius * 2, 800 - this.radius * 2);
+        this.y = randomNumber(600 - this.radius * 2, this.radius * 2);
+        this.dx = randomNumber(0, 1) == 0 ? this.speed : -this.speed;
+        this.dy = randomNumber(0, 1) == 0 ? this.speed : -this.speed;
+    }
+
+    public void update() {
+        x += dx;
+        y += dy;
+        if (hasTouchedVertical()) {
+            dy *= -1;
+        }
+
+        if (hasTouchedHorizontal()) {
+            dx *= -1;
+        }
+    }
+
+    public void draw(Graphics2D bufferEngine) {
+        bufferEngine.setPaint(Color.RED);
+        bufferEngine.fillOval(x, y, this.radius * 2, this.radius * 2);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    private boolean hasTouchedVertical() {
+        return y <= 0 || y >= 600 - this.radius * 2 - this.radius;
+    }
+
+    private boolean hasTouchedHorizontal() {
+        return x <= 0 || x >= 800 - this.radius * 2;
+    }
+
+    public boolean hasTouched() {
+        return hasTouchedVertical() || hasTouchedHorizontal();
+    }
+
+    private int randomNumber(int min, int max) {
+        if (min > max) {
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+}
--- game-engine/src/main/java/doctrina/engine/GameWindow.java
+++ game-engine/src/main/java/doctrina/engine/GameWindow.java
@@ -1,13 +1,26 @@
 package doctrina.engine;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GameWindow extends JFrame {
 
+    private static final int SLEEP = 25;
+    private boolean playing = true;
+    private BufferedImage img;
+    private Graphics2D bufferEngine;
+    private long before;
+    private int score = 0;
+
     private JPanel panel;
+
+    private Ball ball = new Ball(25);
 
     public GameWindow() {
         setSize(800, 600);
@@ -27,5 +40,57 @@ public class GameWindow extends JFrame {
 
     public void start() {
         setVisible(true);
+
+        before = System.currentTimeMillis();
+
+        while (playing) {
+            img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+            RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+            bufferEngine = img.createGraphics();
+            bufferEngine.setRenderingHints(hints);
+
+            update();
+            drawOnBuffer();
+            drawBufferOnScreen();
+
+            try {
+                sleep();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sleep() throws InterruptedException {
+        long sleep = SLEEP - (System.currentTimeMillis() - before);
+        if (sleep < 4) {
+            sleep = 4;
+        }
+        Thread.sleep(sleep);
+        before = System.currentTimeMillis();
+    }
+
+    private void drawBufferOnScreen() {
+        Graphics2D graphics2d = (Graphics2D) panel.getGraphics();
+        graphics2d.drawImage(img, 0, 0, panel);
+        Toolkit.getDefaultToolkit().sync();
+        graphics2d.dispose();
+    }
+
+    private void drawOnBuffer() {
+        ball.draw(bufferEngine);
+
+        bufferEngine.setPaint(Color.WHITE);
+        bufferEngine.drawString("Score: " + score, ball.getX() - 15, ball.getY() - 15);
+    }
+
+    private void update() {
+        ball.update();
+        if (ball.hasTouched()) {
+            score += 10;
+        }
     }
 }
